89ca876e1a6206300557badde1f1ddd493fffb78
r0dinockiy-frontdevelopier <karintsev.roma545@gmail.com>
2025-11-11 10:55:45 +0300

    debug

--- my_library/settings.py
+++ my_library/settings.py
@@ -136,3 +136,16 @@ STATICFILES_DIRS = [
 
 # Для production (опционально)
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+SECRET_KEY = 'ваш-secret-key'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'catalog',
+]
